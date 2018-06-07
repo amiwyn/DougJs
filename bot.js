187d1a3e5a4574b8db7c5d9940160acb7fb07574
Hugues Trottier <Hugues_trottier@hotmail.com>
2018-06-06 21:46:14 -0400

    yeah huh, about not testing things...

--- bot.js
+++ bot.js
@@ -2,18 +2,21 @@ const { IncomingWebhook, RTMClient, WebClient } = require('@slack/client');
 const Configstore = require('configstore');
 
 const config = new Configstore("bot");
-const token = config.get('bot').bot_access_token;
-
 const slurs = ['fuck'];
 
-const web = new WebClient(token);
-const rtm = new RTMClient(token);
+let rtm;
+let web;
 
 module.exports = {
   start() {
     if (this.started) {
       return;
     }
+
+    let token = config.get('bot').bot_access_token;
+    web = new WebClient(token);
+    rtm = new RTMClient(token);
+
     rtm.start();
     rtm.on('message', onMessage);
     //more events
